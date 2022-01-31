aa98247d94a34d990425411799df5d27b3281101
NEONKID <contact@neonkid.xyz>
2022-01-31 17:45:45 +0900

    Remove optional param..

--- modules/author/usecase/addBookToAuthor/event_handler.py
+++ modules/author/usecase/addBookToAuthor/event_handler.py
@@ -13,6 +13,6 @@ class AddBookToAuthorEventHandler(BaseEvent):
     def __init__(self, uc: AddBookToAuthorUseCase) -> None:
         self.uc = uc
 
-    async def handle(self, param: Optional[AuthorAddedToBookDomainEvent] = None) -> None:
+    async def handle(self, param: AuthorAddedToBookDomainEvent = None) -> None:
         command = AddBookToAuthorCommand(book_id=BookId(param.book_id), author_id=AuthorId(param.author_id))
         await self.uc.invoke(command)
